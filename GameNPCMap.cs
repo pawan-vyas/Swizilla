b830126cbc65e57c0c0f2dc47d2c3aecb79572d5
Pawan Vyas <pawan.vyas.gitlab@gmail.com>
2020-07-12 11:31:04 +0530

    Added Swizilla Part 3b

---
+++ GameNPCMap.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Swizilla
+{
+  interface INonPlayableCharacter
+  {
+    void Diplomacy(string playerRace);
+    void Fight(string playerRace);
+  }
+
+  class DwarfGateLord : INonPlayableCharacter
+  {
+    public void Diplomacy(string playerRace)
+    {
+      if ("orc" == playerRace)
+      {
+        Console.WriteLine($"No peace with {playerRace}!");
+        Fight(playerRace);
+      }
+      else
+        Console.WriteLine($"Yes puny {playerRace}"
+           + $", lets resolve this amicably!");
+    }
+
+    public void Fight(string playerRace)
+    {
+      Console.WriteLine($"Now I will beat you to"
+        + $" pulp silly {playerRace}!");
+    }
+  }
+
+  class GameNPCMap
+  {
+    // Map which holds objects of available 
+    // NPC classes. But being referenced by
+    // INonPlayableCharacter.
+    static Dictionary<string, INonPlayableCharacter>
+      npcMap =
+      new Dictionary<string, INonPlayableCharacter>()
+      {
+        { "dwarf", new DwarfGateLord() }
+        // More NPC mappings here.
+      };
+
+    static void Main(string[] args)
+    {
+      string playerRace = "orc";
+      string interactedNPC = "dwarf";
+      string interaction = "Diplomacy";
+
+      // Getting player selected NPC
+      // from npcMap.
+      if (npcMap.TryGetValue(interactedNPC,
+        out INonPlayableCharacter npc))
+      {
+        // Using Reflection, invoking the
+        // player selected interaction
+        // on the selected NPC object.
+        npc.GetType()
+          .GetMethod(interaction)
+          .Invoke(
+              // Object on which to invoke
+              // the method, basically "this"
+              npc,
+
+              // Parameters required by the method.
+              // Has to be object[], and elements in
+              // same sequence as Method signature.
+              new object[] { playerRace }
+            );
+      }
+    }
+  }
+}
---
+++ GameNPCSwitch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+namespace Swizilla
+{
+  class GameNPCSwitch
+  {
+    static void Main(string[] args)
+    {
+      // Defines race type of the player.
+      string playerRace = "orc";
+
+      // Defines race of NPC player
+      // has interacted with.
+      string interactedNPC = "dwarf";
+
+      // Defines type of interaction with NPC
+      // chosen by player.
+      string interaction = "Diplomacy";
+
+      switch (interactedNPC)
+      {
+        case "dwarf":
+          {
+            switch (interaction)
+            {
+              case "Fight":
+                {
+                  Console.WriteLine($"I will beat you to pulp"
+                    + $" silly {playerRace}!");
+                  break;
+                }
+              case "Diplomacy":
+                {
+                  if ("orc" == playerRace)
+                  {
+                    Console.WriteLine($"No peace with {playerRace}!");
+                    // Code for Fighting,
+                    // same as that of case "Fight". :(
+                    Console.WriteLine($"I will beat you to pulp"
+                      + $" silly {playerRace}!");
+                  }
+                  else
+                    Console.WriteLine($"Yes puny {playerRace}"
+                       + $", lets resolve this amicably!");
+                  break;
+                }
+            }
+            break;
+          }
+          // More NPC cases here.
+      }
+    }
+  }
+}
