3750f0f3bb7f160b8b5f56c5b5b2d60f94624589
Pawan Vyas <pawan.vyas.gitlab@gmail.com>
2020-07-12 11:30:49 +0530

    Added Swizilla Part 3a

---
+++ GameActionMap.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+namespace Swizilla
+{
+  class GameActionMap
+  {
+    // Function which saves the current
+    // game data.
+    static void QuickSave()
+    {
+      string gameData = $"Dummy Game Data\n\t" +
+        $"Save Time: {DateTime.Now}" +
+        $"\n\tSaved using ActionMap!";
+      File.WriteAllText("SevFile.sev", gameData);
+      Console.WriteLine("Game data saved!");
+    }
+
+    // Function which loads the game
+    // data from *.sev file.
+    static void QuickLoad()
+    {
+      string loadedData = File.ReadAllText("SevFile.sev");
+      Console.WriteLine($"Saved data loaded from file:" +
+        $"\n{loadedData}");
+    }
+
+    // Function which opens inventory menu.
+    static void OpenInventory()
+    {
+      Console.WriteLine("Inventory loaded with data:");
+      Console.WriteLine("\tHealth Kits: 10" +
+        "\tRevive Kits: 5\tBullets: 124");
+    }
+
+    // Defining a map which holds
+    // Action delegates representing
+    // each action from the game.
+    static Dictionary<string, Action> gameActionMap
+      = new Dictionary<string, Action>()
+      {
+          { "quicksave", QuickSave },
+          { "quickload", QuickLoad },
+          { "inventory", OpenInventory }
+      };
+
+    static void Main(string[] args)
+    {
+      Console.Write("Enter name of action to perform: ");
+
+      // Variable to hold the name of the
+      // user provided action to be performed
+      string actionName = Console.ReadLine().Trim().ToLower();
+
+      if (gameActionMap.ContainsKey(actionName))
+        gameActionMap[actionName]();
+        // Or: gameActionMap[actionName].Invoke();
+      else
+        Console.WriteLine($"No action named: " +
+          $"{actionName} exists.");
+    }
+  }
+}
---
+++ GameActionSwitch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+namespace Swizilla
+{
+  class GameActionSwitch
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Enter name of action to perform: ");
+
+      // Variable to hold the name of the
+      // action entered which will be used
+      // execute that specific code.
+      string actionName = Console.ReadLine().Trim().ToLower();
+
+      // Using switch-case to find which
+      // action is to be executed.
+      switch (actionName)
+      {
+        case "quicksave":
+          string gameData = $"Dummy Game Data\n\t" +
+            $"Save Time: {DateTime.Now}" +
+            $"\n\tSaved using switch-case.";
+          File.WriteAllText("SevFile.sev", gameData);
+          Console.WriteLine("Game data saved!");
+          break;
+
+        case "quickload":
+          string loadedData = File.ReadAllText("SevFile.sev");
+          Console.WriteLine($"Saved data loaded from file:" +
+            $"\n{loadedData}");
+          break;
+
+        case "inventory":
+          Console.WriteLine("Inventory loaded with data:");
+          Console.WriteLine("\tHealth Kits: 10" +
+            "\tRevive Kits: 5\tBullets: 124");
+          break;
+
+        default:
+          Console.WriteLine($"No action named:" +
+            $"{actionName} exists.");
+          break;
+      }
+    }
+  }
+}
