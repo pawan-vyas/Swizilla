80604d4a8dd33827d6a6096a4de283525fef7301
Pawan Vyas <pawan.vyas.gitlab@gmail.com>
2020-07-12 11:29:52 +0530

    Added Swizilla Part 1

---
+++ RainbowArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+namespace Swizilla
+{
+    class RainbowArray
+    {
+        static void Main(string[] args)
+        {
+            // Define an array which contains color positions.
+            string[] rainbowColors = { "unknown", "Red", "Orange", "Yellow",
+                                        "Green", "Blue", "Indigo", "Violet" };
+
+            Console.Write("Mention position(1 to 7) in the Rainbow to get the color: ");
+
+            // Variable to hold the position provided by user.
+            int position = Convert.ToInt32(Console.ReadLine().Trim());
+
+            // Variable to hold the name of Color provided position.
+            string color;
+
+            // Sanity check to verify if position is between 1 to 7.
+            if (1 > position || rainbowColors.Length - 1 < position)
+                // If its not, then simply set colors as unknown,
+                // i.e. element at zeroth-offset in rainbowColors.
+                color = rainbowColors[0];
+            else
+                // Else get color at position-offset from the
+                // rainbowColors array.
+                color = rainbowColors[position];
+
+            // Displaying which color was at provided position.
+            Console.WriteLine($"Color at {position} is: {color}");
+        }
+    }
+}
---
+++ RainbowSwitch.cs
@@ -0,0 +1,48 @@
+﻿using System;
+namespace Swizilla
+{
+    class RainbowSwitch
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Mention position(1 to 7) in the Rainbow to get the color: ");
+            
+            // Variable to hold the user entered position.
+            int position = Convert.ToInt32(Console.ReadLine().Trim());
+            string color;
+
+            // Using switch, check which position was entered,
+            // and assign the appropriate value to color.
+            switch (position)
+            {
+                case 1:
+                    color = "Red";
+                    break;
+                case 2:
+                    color = "Orange";
+                    break;
+                case 3:
+                    color = "Yellow";
+                    break;
+                case 4:
+                    color = "Green";
+                    break;
+                case 5:
+                    color = "Blue";
+                    break;
+                case 6:
+                    color = "Indigo";
+                    break;
+                case 7:
+                    color = "Violet";
+                    break;
+                default:
+                    color = "unknown";
+                    break;
+            }
+
+            // Displaying which color was at provided position.
+            Console.WriteLine($"Color at {position} is: {color}");
+        }
+    }
+}
