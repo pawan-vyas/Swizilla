98bed8953acbfc16b71745c455bfa2b27de627ec
Pawan Vyas <pawan.vyas.gitlab@gmail.com>
2020-07-12 11:30:27 +0530

    Added Swizilla Part 2

---
+++ VideoGameMap.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Swizilla
+{
+  class VideoGameMap
+  {
+    static void Main(string[] args)
+    {
+      // Defining code to action map.
+      var codeActionMap = new Dictionary<int, string>()
+      {
+        { 12 , "jump" },
+        { 23 , "crouch" },
+        { 34, "quicksave" },
+        { 45, "quickload" },
+        { 56, "inventory" },
+        { 67, "skilltree" }
+      };
+
+      Console.Write("Enter Key Code to get its Action Name: ");
+
+      // Variable used to get name
+      // of action assigned to it.
+      int keyCode = Convert.ToInt32(Console.ReadLine().Trim());
+
+      // Variable to hold human-readable
+      // name of the above keyCode.
+      // Default assigned to unknown.
+      string actionName = "unknown";
+
+      // Now we'll simply read from the map
+      // whether given keyCode exists or not
+      // and re-assign the action name to the
+      // variable based on its code from map.
+      if (codeActionMap.ContainsKey(keyCode))
+        actionName = codeActionMap[keyCode];
+
+      Console.WriteLine($"Action assigned to {keyCode}" +
+          $" is {actionName}.");
+    }
+  }
+}
---
+++ VideoGameSwitch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace Swizilla
+{
+  class VideoGameSwitch
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Enter Key Code to get its Action Name: ");
+
+      // Variable used to get name
+      // of action assigned to it.
+      int keyCode = Convert.ToInt32(Console.ReadLine().Trim());
+
+      // Variable to hold human-readable
+      // name of the above keyCode.
+      string actionName;
+
+      // Using switch-case to get the
+      // keyCode's mappingName.
+      switch (keyCode)
+      {
+        case 12:
+          actionName = "jump";
+          break;
+        case 23:
+          actionName = "crouch";
+          break;
+        case 34:
+          actionName = "quicksave";
+          break;
+        case 45:
+          actionName = "quickload";
+          break;
+        case 56:
+          actionName = "inventory";
+          break;
+        case 67:
+          actionName = "skilltree";
+          break;
+        default:
+          actionName = "unknown";
+          break;
+      }
+
+      Console.WriteLine($"Action assigned to {keyCode}" +
+          $" is {actionName}.");
+    }
+  }
+}
